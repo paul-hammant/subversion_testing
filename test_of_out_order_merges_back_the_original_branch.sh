4d03b1a475e30d37e31351e9d898e4c500f47a3c
Paul Hammant <paul@hammant.org>
2016-06-18 06:34:29 -0400

    fossil version

--- test_of_out_order_merges_back_the_original_branch.sh
+++ test_of_out_order_merges_back_the_original_branch.sh
@@ -1,45 +1,47 @@
 #!/bin/sh
 
-last_rev () { 
-git log | grep "^commit " | head -n1 | cut -d' ' -f2
-} 
+last_rev () {
+fossil finfo testfile.txt | grep "branch:" | head -n1 | cut -d' ' -f2 | cut -d'[' -f2 | cut -d']' -f1
+}
 
 ./test_of_out_order_merges_from_one_branch_to_another.sh > /dev/null
 
+set -e
+
 cd data
-git checkout two
+fossil checkout two
 perl -pi -e 's/^a/a !!/' testfile.txt
-git commit -am "line a changed again"
-a_changed_in_br_two=$(last_rev) 
+fossil commit -m "line a changed again"
+a_changed_in_br_two=$(last_rev)
 perl -pi -e 's/^b/b !!/' testfile.txt
-git commit -am "line b changed again"
-b_changed_in_br_two=$(last_rev) 
+fossil commit -m "line b changed again"
+b_changed_in_br_two=$(last_rev)
 perl -pi -e 's/^c/c !!/' testfile.txt
-git commit -am "line c changed again"
-c_changed_in_br_two=$(last_rev) 
+fossil commit -m "line c changed again"
+c_changed_in_br_two=$(last_rev)
 perl -pi -e 's/^d/d !!/' testfile.txt
-git commit -am "line d changed again"
-d_changed_in_br_two=$(last_rev) 
+fossil commit -m "line d changed again"
+d_changed_in_br_two=$(last_rev)
 perl -pi -e 's/^e/e !!/' testfile.txt
-git commit -am "line e changed again"
+fossil commit -m "line e changed again"
 e_changed_in_br_two=$(last_rev)
-git checkout one
-git cherry-pick --no-commit $a_changed_in_br_two 
-git commit -m "line a merged into one"
-git cherry-pick --no-commit $e_changed_in_br_two 
-git commit -m "line e merged into one"
-git cherry-pick --no-commit $c_changed_in_br_two 
-git commit -m "line c merged into one"
-git cherry-pick --no-commit $d_changed_in_br_two 
-git commit -m "line d merged into one"
-git cherry-pick --no-commit $b_changed_in_br_two 
-git commit -m "line b merged into one"
+fossil checkout one
+fossil merge --cherrypick $a_changed_in_br_two
+fossil commit -m "line a merged into one"
+fossil merge --cherrypick $e_changed_in_br_two
+fossil commit -m "line e merged into one"
+fossil merge --cherrypick $c_changed_in_br_two
+fossil commit -m "line c merged into one"
+fossil merge --cherrypick $d_changed_in_br_two
+fossil commit -m "line d merged into one"
+fossil merge --cherrypick $b_changed_in_br_two
+fossil commit -m "line b merged into one"
 echo "Contents of testfile.txt on branch 'one':"
 more testfile.txt
-git checkout two
+fossil checkout two
 echo "Contents of testfile.txt on branch 'two' (they are the same, as a result of merges):"
 more testfile.txt
-git checkout one
+fossil checkout one
 echo "Attempt redundant merge of branch two into branch one (individual commits are merged already, and files at HEAD revision are idential):"
-git merge two  --no-commit
-git commit -m "remainder (whatever that means) merged into one"
+fossil merge two
+fossil commit -m "remainder (whatever that means) merged into one"
--- test_of_out_order_merges_from_one_branch_to_another.sh
+++ test_of_out_order_merges_from_one_branch_to_another.sh
@@ -1,47 +1,49 @@
 #!/bin/sh
 
-last_rev () { 
-git log | grep "^commit " | head -n1 | cut -d' ' -f2
-} 
+last_rev () {
+fossil finfo testfile.txt | grep "branch:" | head -n1 | cut -d' ' -f2 | cut -d'[' -f2 | cut -d']' -f1
+}
 
 set -e
 
 mkdir data
 cd data
-git init
-echo "a\n\nb\n\nc\n\nd\n\ne\n" > testfile.txt
-git add testfile.txt
-git commit -m "branch_one_creation"
-git branch -m  one
-git branch two
-git checkout two
-# git commit -m "branch_two_creation" <-- unecessary in Git
-git checkout one
+fossil init .fossilDB
+fossil open .fossilDB
+fossil branch new one trunk
+fossil checkout one
+echo "a\\n\\nb\\n\\nc\\n\\nd\\n\\ne\\n" > testfile.txt
+fossil add testfile.txt
+fossil commit -m "branch_one_creation"
+fossil branch new two one
+fossil checkout two
+fossil commit --allow-empty -m "branch_two_creation"
+fossil checkout one
 perl -pi -e 's/^a/a aa/' testfile.txt
-git commit -am "line a changed"
-a_changed_in_br_one=$(last_rev) 
+fossil commit -m "line a changed"
+a_changed_in_br_one=$(last_rev)
 perl -pi -e 's/^b/b bbb/' testfile.txt
-git commit -am "line b changed"
-b_changed_in_br_one=$(last_rev) 
+fossil commit -m "line b changed"
+b_changed_in_br_one=$(last_rev)
 perl -pi -e 's/^c/c cccc/' testfile.txt
-git commit -am "line c changed"
-c_changed_in_br_one=$(last_rev) 
+fossil commit -m "line c changed"
+c_changed_in_br_one=$(last_rev)
 perl -pi -e 's/^d/d ddddd/' testfile.txt
-git commit -am "line d changed"
-d_changed_in_br_one=$(last_rev) 
+fossil commit -m "line d changed"
+d_changed_in_br_one=$(last_rev)
 perl -pi -e 's/^e/e eeeeee/' testfile.txt
-git commit -am "line e changed"
+fossil commit -m "line e changed"
 e_changed_in_br_one=$(last_rev)
-git checkout two
-git cherry-pick --no-commit $a_changed_in_br_one
-git commit -am "line a merged into two"
-git cherry-pick --no-commit $e_changed_in_br_one 
-git commit -am "line e merged into two"
-git cherry-pick --no-commit $c_changed_in_br_one  
-git commit -am "line c merged into two"
-git cherry-pick --no-commit $d_changed_in_br_one  
-git commit -am "line d merged into two"
-git cherry-pick --no-commit $b_changed_in_br_one  
-git commit -am "line b merged into two"
-git merge one --no-commit
-git commit -m "remainder (whatever that means) merged into two"
+fossil checkout two
+fossil merge --cherrypick $a_changed_in_br_one
+fossil commit -m "line a merged into two"
+fossil merge --cherrypick $e_changed_in_br_one
+fossil commit -m "line e merged into two"
+fossil merge --cherrypick $c_changed_in_br_one
+fossil commit -m "line c merged into two"
+fossil merge --cherrypick $d_changed_in_br_one
+fossil commit -m "line d merged into two"
+fossil merge --cherrypick $b_changed_in_br_one
+fossil commit -m "line b merged into two"
+fossil merge one
+fossil commit -m "remainder (whatever that means) merged into two"
